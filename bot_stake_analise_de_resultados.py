b981669b6d2c3f70d89dfb4cea33d21578154540
JFelipe <jeffersonfelipe.nm@hotmail.com>
2022-12-17 08:30:54 -0300

    BOT RESULTADO

--- bot_stake_analise_de_resultados.py
+++ bot_stake_analise_de_resultados.py
@@ -10,7 +10,7 @@ game_seed = '0000000000000000001b34dc6a1e86083f95500b096231436e9b25cbdd0075c4'
 first_game = '78a9757d3be42b74a3f70239078ad9317125fe9ee630d5bdada46de963e56752'
 
 # Última  hash inserida pelo usuário
-game_hash = 'c870adbf72a66eff98959d8e759ea2d959ba6087d5078f30253dd9a86266b6e7'  # Mude esse valor
+game_hash = '1f93980af45e9f01a6dacf9d203eb568065994fd7e2039a771dd86f6a99e36e6'  # Mude esse valor
 
 
 def get_result(gamehash, seed):
@@ -45,15 +45,20 @@ ordem = []
 resultado = []
 banca = []
 
-crashpoint = 5
-stake = 0.01
+crashpoint = 5  # Quanto x deseja parar
+stake = 0.002  # Stake fixa
+casas_para_gale = 40  # quantas vezes desejar fazer gale
+nivel_gale = 1.25
 
-# Simular Gale buscando 5x
+stake_gale = stake
+
+
 saldo_simulado = 60
 banca_simulada = []
-stake_gale = 0.002
-nao_ocorrencia_5x = 0
-ocorrencias_5x_dif = [0]
+
+
+nao_ocorrencia = 0
+
 
 contador = 0  # Está sendo utilizado para ser manipulado pela var ordem
 
@@ -65,63 +70,70 @@ ocorrencia_salva = [0]
 
 for i in amostragem:
 
-    if i >= crashpoint:
+    if i < crashpoint:
+        contador += 1
+        ordem.append(contador)  # ordem
+        rate.append((ocorrencia / contador) * 100)
+        nao_ocorrencia += 1
 
-        if nao_ocorrencia_5x > 40:
-            contador += 1
-            stake_gale = 0.002
-            saldo_simulado = round(saldo_simulado - stake_gale, 4)
+        if nao_ocorrencia > casas_para_gale:
+            stake_gale = stake
+            resultado.append(-stake_gale)
 
-            nao_ocorrencia_5x = 0
+            saldo_simulado = round(saldo_simulado - stake_gale, 8)
             banca_simulada.append(saldo_simulado)
+
+            nao_ocorrencia = 1
+            ordem_de_ocorrencia.append(nao_ocorrencia)
+
         else:
-            contador += 1
+
+            saldo_simulado = round(saldo_simulado - stake_gale, 8)
+            banca_simulada.append(saldo_simulado)
+            resultado.append(-stake_gale)
+
+            stake_gale = stake_gale * nivel_gale
+
+            ordem_de_ocorrencia.append(nao_ocorrencia)
+
+    else:  # Se maior que crashpoint
+        contador += 1
+        ocorrencia += 1  # rate
+        rate.append((ocorrencia / contador) * 100)
+
+        if nao_ocorrencia == casas_para_gale:
+
             ordem.append(contador)  # ordem
-
             ocorrencia_salva.append(contador)
-            diferenca_do_ultimo_aparecimento = contador - ocorrencia_salva[-2]
-            ordem_de_ocorrencia.append(diferenca_do_ultimo_aparecimento)
-
-            resultado.append((crashpoint * stake) - stake)
-            # saldo = round(saldo + ((crashpoint * stake) - stake), 2)
-            # banca.append(saldo)
 
             ocorrencia += 1  # rate
             rate.append((ocorrencia / contador) * 100)
 
-            # buscando gale no 5x
+            stake_gale = stake
+            saldo_simulado = round(saldo_simulado + ((crashpoint * stake_gale) - stake_gale), 8)
 
-            ocorrencias_5x_dif.append(nao_ocorrencia_5x)
-            ocorrencia_5x = nao_ocorrencia_5x - ocorrencias_5x_dif[-2]
-            ocorrencias_5x_dif.append(ocorrencia_5x)
-            nao_ocorrencia_5x = 0
+            nao_ocorrencia = 1
+            ordem_de_ocorrencia.append(nao_ocorrencia)
 
-            saldo_simulado = round(saldo_simulado + ((crashpoint * stake_gale) - stake_gale), 2)
-            stake_gale = 0.002
-            banca_simulada.append(saldo_simulado)
-
-    else:
-        contador += 1
-        ordem.append(contador)  # ordem
-        ordem_de_ocorrencia.append(0)
-
-        resultado.append(-stake)
-        rate.append((ocorrencia / contador) * 100)
-
-        if nao_ocorrencia_5x > 40:
-
-            stake_gale = 0.002
-            saldo_simulado = round(saldo_simulado - stake_gale, 4)
-            nao_ocorrencia_5x = 0
+            resultado.append((crashpoint * stake_gale) - stake)
             banca_simulada.append(saldo_simulado)
 
         else:
-            nao_ocorrencia_5x += 1
-            ocorrencias_5x_dif.append(nao_ocorrencia_5x)
 
-            saldo_simulado = round(saldo_simulado - stake_gale, 4)
-            stake_gale = stake_gale * 1.25
+            ordem.append(contador)  # ordem
+
+            ordem_de_ocorrencia.append(nao_ocorrencia + 1)
+
+            resultado.append((crashpoint * stake_gale) - stake_gale)
+
+            # buscando gale no 5x
+            nao_ocorrencia = 0
+
+            saldo_simulado = round(saldo_simulado + ((crashpoint * stake_gale) - stake_gale), 8)
             banca_simulada.append(saldo_simulado)
+
+            stake_gale = stake
+
 
 qtd_de_ocorrencias.append(ocorrencia)
 media_rate = (ocorrencia / contador) * 100
@@ -130,7 +142,6 @@ df = pd.DataFrame(
     list(zip(ordem, amostragem, resultado, rate, ordem_de_ocorrencia, banca_simulada)),
     columns=['ORDEM', 'CRASHPOINT', 'RESULTADO', 'RATE', 'DIF OCORRENCIA', 'BANCA SIMULADA'])
 
-# df1 = pd.DataFrame(list(zip(ordem)))
 
 media_diferenca_de_ocorrencia = sum(ordem_de_ocorrencia) / ocorrencia
 quantidade_de_itens_com_dif_acima_da_media = len([i for i in ordem_de_ocorrencia if i > media_diferenca_de_ocorrencia])
@@ -140,29 +151,9 @@ print(f'Foram analisados {len(amostragem)} e tivemos {ocorrencia}\n ')
 print(f'Média de ocorrencia de foi {round(media_rate, 4)}%\n ')
 print(f'A média da diferença de ocorrência é {(sum(ordem_de_ocorrencia)) / ocorrencia}\n ')
 print(f'A quantidade de itens acima da média de dif foram {quantidade_de_itens_com_dif_acima_da_media}')
+print(max(amostragem))
 
 # Gráfico
 plt.title('EVOLUÇÃO DA BANCA- 5X - ENTRANDO SEMPRE')
 plt.plot(list(df['ORDEM']), df['BANCA SIMULADA'])
 plt.show()
-
-
-# Análise probabilística
-
-
-def analise_probabilidades():
-    negatives = []
-    in_a_row = 0
-    for item in results:
-        if item < crashpoint:
-            in_a_row += 1
-        else:
-            in_a_row = 0
-        negatives.append(in_a_row)
-    negatives = np.array(negatives)
-
-    for i in range(1, 41):
-        print(f'Probabilidade de perder {i} jogo(s) consecutivos: {round(((negatives >= i).mean()) * 100, 3)} %')
-
-
-analise_probabilidades()
